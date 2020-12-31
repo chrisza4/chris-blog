50b2950228cc449fd94c17646fd29881785c41cc
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2020-12-31 11:59:27 +0700

    NavBar menu item

--- src/components/layout/navBar.js
+++ src/components/layout/navBar.js
@@ -1,9 +1,18 @@
 import React from "react"
-import { StyledDiv } from "../tools/styled"
+import { StyledDiv, StyledUl, StyledLi } from "../tools/styled"
 import styles from "./navBar.module.css"
 
 const NavBarPlaceHolder = StyledDiv({ className: styles.navBar })
+const NavBarMenu = StyledUl({ className: styles.navBarMenus })
+const NavBarMenuItemPlaceHolder = StyledLi({ className: styles.navBarMenu })
 
 export default function NavBar() {
-  return <NavBarPlaceHolder>Hi</NavBarPlaceHolder>
+  return (
+    <NavBarPlaceHolder>
+      <NavBarMenu>
+        <NavBarMenuItemPlaceHolder>aaa</NavBarMenuItemPlaceHolder>
+        <NavBarMenuItemPlaceHolder>bbb</NavBarMenuItemPlaceHolder>
+      </NavBarMenu>
+    </NavBarPlaceHolder>
+  )
 }
