0ca8dadae7dac7c35060ab1b16aa8140e6a381c9
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2023-02-26 18:14:45 +0700

    Try add react query

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,3 +8,10 @@ import "./src/style.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
+
+import React from "react"
+import RootElement from "./src/components/rootElement"
+
+export const wrapRootElement = ({ element }) => {
+  return <RootElement>{element}</RootElement>
+}
